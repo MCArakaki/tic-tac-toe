b3006a62445c9546f65e0824c3c05db322400dd7
MCArakaki <91706248+MCArakaki@users.noreply.github.com>
2021-10-09 13:29:47 +0900

    機能追加 ・〇×を交互に表示 ・３×３のマス目表示

--- src/App.jsx
+++ src/App.jsx
@@ -2,55 +2,73 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // const [displayInfo, setDisplayInfo] = useState(new Array(9).fill(""));
   const [displayInfo, setDisplayInfo] = useState([
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
   ]);
 
-  const [turnInfo, setTurnInfo] = useState("O");
+  const [turnInfo, setTurnInfo] = useState(true);
+
+  const complete_patterns = [[0], [1], [2]];
+  const check_true = [];
+  const check_false = [];
 
   const onClickButton = (index) => {
-    if (turnInfo === "O") {
-      // 〇の番の時
-      displayInfo[index] = "O";
-      setTurnInfo("X");
-    } else {
-      // ×の番の時
-      displayInfo[index] = "X";
-      setTurnInfo("O");
-    }
-
+    //const judgeArr = []
+    // 現在の画面情報を一旦コピー
     const newDisplayInfo = [...displayInfo];
+    // クリックされたボタンの位置に、〇か×をセット
+    newDisplayInfo[index] = turnInfo === true ? "〇" : "×";
+    // 順番交代
+    setTurnInfo(!turnInfo);
+    // 画面更新
     setDisplayInfo(newDisplayInfo);
+
+    // 勝敗チェック
   };
+
+  const handleClickReset = () => {
+    // 初期化
+    setDisplayInfo(new Array(9).fill(""));
+    setTurnInfo(true);
+  };
+
   return (
     <>
       <div>
-        <p>〇×ゲーム</p>
+        <p className="title">〇×ゲーム</p>
       </div>
       <div>
-        <p>{turnInfo}の番です</p>
+        <p className="turn">{turnInfo === true ? "〇" : "×"}の番だよ</p>
       </div>
-      {displayInfo.map((display, index) => {
-        return (
-          <div key={index}>
+      <div className="grid_squares">
+        {displayInfo.map((display, index) => {
+          return (
             <button
+              key={index}
+              className="squares"
               onClick={() => {
                 onClickButton(index);
               }}
             >
-              {displayInfo}
+              {display}
             </button>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <div className="wrapper_reset">
+        <button className="reset" onClick={handleClickReset}>
+          リセット
+        </button>
+      </div>
     </>
   );
 };
